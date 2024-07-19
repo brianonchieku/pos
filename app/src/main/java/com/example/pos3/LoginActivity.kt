07a69a07c437e2044ec1ff75155a9f980c1a7bfc
brianonchieku <brianonchikeku0@gmail.com>
2024-07-19 12:42:40 +0300

    fixed the table: added subtotal

--- app/src/main/java/com/example/pos3/LoginActivity.kt
+++ app/src/main/java/com/example/pos3/LoginActivity.kt
@@ -50,7 +50,6 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.pos3.ui.theme.Pos3Theme
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
--- app/src/main/java/com/example/pos3/SalesActivity.kt
+++ app/src/main/java/com/example/pos3/SalesActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -15,21 +16,29 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -44,6 +53,7 @@ import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -94,35 +104,42 @@ fun Sales(products: List<ProductSale>,items: List<String>) {
                 }
             }
 
-            Column {
-                SearchBar(query, expanded, onQueryChanged = {
-                    query = it
-                    expanded = true
-                }, onTextFieldFocused = { expanded = true })
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 40.dp),
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Box(modifier = Modifier.wrapContentSize(), contentAlignment = Alignment.Center) {
+                    SearchBar(query, expanded, onQueryChanged = {
+                        query = it
+                        expanded = true
+                    }, onTextFieldFocused = { expanded = true })
 
-                DropdownMenu(
-                    expanded = expanded && filteredItems.isNotEmpty(),
-                    onDismissRequest = { expanded = false }
-                ) {
-                    filteredItems.forEach { item ->
-                        DropdownMenuItem(
-                            text = { Text(text = item) },
-                            onClick = {
-                                val selectedProduct = products.firstOrNull { it.name == item }
-                                if (selectedProduct != null) {
-                                    selectedProducts = selectedProducts + selectedProduct
+                    DropdownMenu(
+                        expanded = expanded && filteredItems.isNotEmpty(),
+                        onDismissRequest = { expanded = false }
+                    ) {
+                        filteredItems.forEach { item ->
+                            DropdownMenuItem(
+                                text = { Text(text = item) },
+                                onClick = {
+                                    val selectedProduct = products.firstOrNull { it.name == item }
+                                    if (selectedProduct != null) {
+                                        selectedProducts = selectedProducts + selectedProduct
+                                    }
+                                    query = item
+                                    expanded = false
                                 }
-                                query = item
-                                expanded = false
-                            }
-                        )
+                            )
+                        }
                     }
                 }
+
                 ProductTable(products = selectedProducts, onQuantityChange = { product, newQuantity ->
                     selectedProducts = selectedProducts.map {
                         if (it.name == product.name) it.copy(quantity = newQuantity) else it
                     }.filter { it.quantity>0 } //remove item from table
                 })
+                
             }
         }
     }
@@ -134,7 +151,7 @@ fun SearchBar(query: String,expanded: Boolean, onQueryChanged: (String) -> Unit,
     val focusManager = LocalFocusManager.current
     val keyboardController = LocalSoftwareKeyboardController.current
 
-    TextField(
+    OutlinedTextField(
         value = query,
         onValueChange = onQueryChanged,
         label = { Text("Search") },
@@ -145,10 +162,11 @@ fun SearchBar(query: String,expanded: Boolean, onQueryChanged: (String) -> Unit,
             .clickable {
                 focusRequester.requestFocus()
                 keyboardController?.show()
+                onTextFieldFocused()
             },
 
         leadingIcon = {
-            Icon(Icons.Default.Search, contentDescription = "Search Icon")
+            Icon(Icons.Default.Search, contentDescription = "Search Icon", modifier = Modifier.clickable {  onTextFieldFocused() })
         }
     )
     LaunchedEffect(expanded) {
@@ -161,10 +179,28 @@ fun SearchBar(query: String,expanded: Boolean, onQueryChanged: (String) -> Unit,
         }
     }
 }
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductTable(products: List<ProductSale>, onQuantityChange: (ProductSale, Int) -> Unit) {
-
     val tableHeaders = listOf("Name", "Quantity", "Price", "Total")
+    val subtotal = products.sumOf { it.price * it.quantity }
+    var paymentMethod by remember { mutableStateOf("") }
+
+    var expanded by remember { mutableStateOf(false) }
+    val list = listOf(
+        "Cash",
+        "Mobile Payment"
+    )
+    val icon = if (expanded){
+        Icons.Filled.KeyboardArrowUp
+    }else{
+        Icons.Filled.KeyboardArrowDown
+    }
+
+
+
+
+
     Column(modifier = Modifier
         .fillMaxSize()
         .padding(16.dp)) {
@@ -185,41 +221,91 @@ fun ProductTable(products: List<ProductSale>, onQuantityChange: (ProductSale, In
                 Text(text = product.name, modifier = Modifier
                     .weight(1f)
                     .padding(8.dp))
-                Text(text = product.quantity.toString(), modifier = Modifier
-                    .weight(1f)
-                    .padding(8.dp))
+                Row(modifier = Modifier.weight(1f),verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = product.quantity.toString(), modifier = Modifier
+                        .padding(8.dp))
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(start = 2.dp, end = 2.dp)
+                    ) {
+                        IconButton(onClick = {
+                            onQuantityChange(product, product.quantity + 1)
+                        },
+                            modifier = Modifier
+                                .size(10.dp)) {
+                            Icon(painter = painterResource(id = R.drawable.baseline_keyboard_arrow_up_24), contentDescription =null )
+                        }
+                        IconButton(onClick = {
+                            if (product.quantity > 1) {
+                                onQuantityChange(product, product.quantity - 1)
+                            }else {
+                                // Remove the item if quantity is 1
+                                onQuantityChange(product, 0)
+                            }
+                        },
+                            modifier = Modifier
+                                .size(10.dp)) {
+                            Icon(painter = painterResource(id = R.drawable.baseline_keyboard_arrow_down_24), contentDescription = null)
+                        }
+                    }
+                }
+
                 Text(text = product.price.toString(), modifier = Modifier
                     .weight(1f)
                     .padding(8.dp))
                 Text(text = (product.price * product.quantity).toString(), modifier = Modifier
                     .weight(1f)
                     .padding(8.dp))
-                Row(modifier = Modifier.weight(1f)) {
-                    IconButton(onClick = {
-                        if (product.quantity > 1) {
-                            onQuantityChange(product, product.quantity - 1)
-                        }else {
-                            // Remove the item if quantity is 1
-                            onQuantityChange(product, 0)
-                        }
-                    },
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(8.dp)) {
-                        Icon(painter = painterResource(id = R.drawable.baseline_minimize_24), contentDescription = null)
-                    }
-                    IconButton(onClick = {
-                        onQuantityChange(product, product.quantity + 1)
-                    },
-                        modifier = Modifier
-                            .weight(1f)
-                            .padding(8.dp)) {
-                        Icon(imageVector = Icons.Default.Add, contentDescription = null)
-                    }
-                }
             }
             Divider(color = Color.Gray, thickness = 0.5.dp)
         }
+        Row(modifier = Modifier.padding(vertical = 8.dp)) {
+            Spacer(modifier = Modifier.weight(2f))
+            Text(
+                text = "Subtotal: ",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp),
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = subtotal.toString(),
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp),
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Spacer(modifier = Modifier.size(20.dp))
+        Box(modifier = Modifier
+            .wrapContentSize()
+            .background(colorResource(id = R.color.white)), contentAlignment = Alignment.Center) {
+            TextField(value =paymentMethod , onValueChange ={paymentMethod=it},
+                label = {
+                    Text(text = "select payment method")
+                }, trailingIcon = {
+                    Icon(icon, "", modifier = Modifier.clickable { expanded=!expanded } )
+                }, modifier = Modifier.wrapContentSize().background(color = colorResource(id = R.color.white)),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent))
+            DropdownMenu(expanded = expanded,
+                onDismissRequest = { expanded=false}
+            ) {
+                list.forEach { label ->
+                    DropdownMenuItem(text = {Text(text = label) }, onClick = {
+                        paymentMethod=label
+                        expanded=false
+                    })
+
+                }
+
+            }
+            
+        }
+        
     }
 }
 suspend fun fetchProducts(): List<ProductSale> {
